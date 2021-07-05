de49f8040ca85e594c57407800be5d48925c28d4
Anthony Couture <antobox28@gmail.com>
2021-07-06 00:01:49 +0200

    first commit

---
+++ api-course/src/main/java/fr/couture/course/ApiCourseApplication.java
@@ -0,0 +1,13 @@
+package fr.couture.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCourseApplication.class, args);
+	}
+
+}
