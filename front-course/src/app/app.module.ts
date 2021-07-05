e279c7b38ddad2db2486d05f0f16f8453bf337d1
Anthony Couture <antobox28@gmail.com>
2021-07-06 00:41:47 +0200

    add router

--- front-course/src/app/app.module.ts
+++ front-course/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+
+const routes = [
+  {path: '', component: AccueilComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
