99266e9b08c48d1008e50d98b6362f967c3177e7
Anthony Couture <antobox28@gmail.com>
2021-07-06 00:47:52 +0200

    add context path and first controller

---
+++ api-course/src/main/java/fr/couture/course/controllers/AccueilController.java
@@ -0,0 +1,15 @@
+package fr.couture.course.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AccueilController {
+
+    @GetMapping
+    public String test() {
+        return "hello";
+    }
+}
