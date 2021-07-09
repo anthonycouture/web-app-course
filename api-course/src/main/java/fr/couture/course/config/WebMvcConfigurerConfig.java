7df066929efc9d99a6f20447d6c31ef998572ebf
Anthony Couture <antobox28@gmail.com>
2021-07-10 00:17:33 +0200

    add cors

---
+++ api-course/src/main/java/fr/couture/course/config/WebMvcConfigurerConfig.java
@@ -0,0 +1,23 @@
+package fr.couture.course.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebMvcConfigurerConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
