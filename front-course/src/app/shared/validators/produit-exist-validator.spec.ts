05b62a8171138883224a30425e5b12c91cd99e5d
Anthony Couture <antobox28@gmail.com>
2021-07-27 19:45:27 +0200

    add validator custom

---
+++ front-course/src/app/shared/validators/produit-exist-validator.spec.ts
@@ -0,0 +1,7 @@
+import {ProduitExistValidator} from './produit-exist-validator';
+
+describe('ProduitExistValidator', () => {
+  it('should create an instance', () => {
+    expect(new ProduitExistValidator()).toBeTruthy();
+  });
+});
