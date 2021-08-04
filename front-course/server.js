58539f19da4238ebd36bce7017ad7effc4d65192
Anthony Couture <antobox28@gmail.com>
2021-08-04 12:28:41 +0200

    update serveur front

--- front-course/server.js
+++ front-course/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 function requireHTTPS(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
@@ -8,6 +10,7 @@ function requireHTTPS(req, res, next) {
 
 const express = require('express');
 const app = express();
+// Ligne du dessous à mettre en commentaire pour le localhost
 app.use(requireHTTPS);
 app.use(express.static('./dist/front-course'));
 app.get('/*', function (req, res) {
@@ -17,4 +20,5 @@ app.get('/*', function (req, res) {
     }
   );
 });
-app.listen(process.env.PORT || 4200);
+const appListen = app.listen(process.env.PORT || 4200);
+console.log('Le serveur fonctionne sur le port ' + appListen.address().port);
