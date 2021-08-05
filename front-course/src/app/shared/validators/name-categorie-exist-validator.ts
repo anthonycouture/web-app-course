d0d49ed041a59efd5c5f216c28cf45907285fee3
Anthony Couture <antobox28@gmail.com>
2021-08-05 17:38:35 +0200

    opti validator

--- front-course/src/app/shared/validators/name-categorie-exist-validator.ts
+++ front-course/src/app/shared/validators/name-categorie-exist-validator.ts
@@ -1,15 +1,9 @@
 import {Injectable} from "@angular/core";
 import {AbstractControl, ValidationErrors, ValidatorFn} from "@angular/forms";
-import {Categorie} from "../../core/models/categorie";
-import {Observable} from "rxjs";
-import {take} from "rxjs/operators";
 import {CategoriesStoreService} from "../../core/state/categories-store.service";
 
 @Injectable({providedIn: 'root'})
 export class NameCategorieExistValidator {
-
-  // @ts-ignore
-  private _categories$: Observable<Categorie[]> = this._categoriesStore.categories$;
 
   constructor(private _categoriesStore: CategoriesStoreService) {
   }
@@ -20,15 +14,13 @@ export class NameCategorieExistValidator {
       ctrl: AbstractControl
     ): ValidationErrors | null => {
       let exist = false;
-      this._categories$.pipe(take(1)).subscribe((categories) => {
-          categories.forEach(categorie => {
-            if (categorie.nom === ctrl.value && (idCategorie === null || idCategorie !== categorie.id)) {
-              exist = true;
-              return;
-            }
-          });
+      this._categoriesStore.getCategories().forEach(categorie => {
+        if (categorie.nom === ctrl.value && (idCategorie === null || idCategorie !== categorie.id)) {
+          exist = true;
+          return;
         }
-      );
+      });
+
       return exist ? {notValid: true} : null;
     }
   }
--- front-course/src/app/shared/validators/name-produit-exist-validator.ts
+++ front-course/src/app/shared/validators/name-produit-exist-validator.ts
@@ -1,18 +1,13 @@
 import {Injectable} from "@angular/core";
 import {AbstractControl, ValidationErrors, ValidatorFn} from "@angular/forms";
-import {Produit} from "../../core/models/produit";
 import {CategoriesStoreService} from "../../core/state/categories-store.service";
 
 @Injectable({providedIn: 'root'})
 export class NameProduitExistValidator {
 
-  private _produits: Produit[] = [];
 
   constructor(private _categoriesStore: CategoriesStoreService) {
-    // @ts-ignore
-    this._categoriesStore.categories$.subscribe(
-      (data) => this._produits = data
-    );
+
   }
 
 
@@ -20,13 +15,17 @@ export class NameProduitExistValidator {
     return (
       ctrl: AbstractControl
     ): ValidationErrors | null => {
+
       let exist = false;
-      this._produits.forEach(produit => {
-        if (produit.nom === ctrl.value && (idProduit === null || idProduit !== produit.id)) {
-          exist = true;
-          return;
-        }
-      });
+
+      this._categoriesStore.getCategories().forEach((categorie) =>
+        categorie.produits?.forEach(produit => {
+          if (produit.nom === ctrl.value && (idProduit === null || idProduit !== produit.id)) {
+            exist = true;
+            return;
+          }
+        })
+      );
 
       return exist ? {notValid: true} : null;
     }
